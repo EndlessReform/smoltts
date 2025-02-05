74ee3c48b07bb50a0848d7b7a9db70eaf3b9ad73
Jacob Keisling <jacob@keisling.me>
2025-02-04 23:15:53 -0600

    Flatten depthwise output

--- train/convert_safetensors.py
+++ train/convert_safetensors.py
@@ -7,6 +7,11 @@ def main():
     data = torch.load(sys.argv[1])
     model = data["model_state_dict"]
     model = {key.replace("_orig_mod.", ""): value for key, value in model.items()}
+    if model["fast_output.weight"].ndim == 3:
+        print(f"Flattening 3D output projection {model['fast_output.weight'].shape}")
+        model["fast_output.weight"] = (
+            model["fast_output.weight"].transpose(0, 1).flatten(1)
+        )
     save_file(model, "model.safetensors")
 
 
